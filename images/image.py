dbada102f73750bec8ac7cf43bf09df14203097f
brocode <kelimbth@gmail.com>
2023-12-19 15:10:20 +0300

    users can download avatars 📥

--- images/image.py
+++ images/image.py
@@ -6,3 +6,16 @@ def update_avatar_image(img):
     img_col[1].image(img, caption='Your avatar', use_column_width='auto')   # wrap the image in the center column
     
     return img
+
+def download_avatar(avatar_image):
+    """ This function allows one to download their avatars."""
+
+    with open(avatar_image, 'rb') as file:
+        st.download_button(
+            label='Download avatar',
+            type="primary",
+            data=file,
+            file_name=avatar_image,
+            use_container_width=True
+        )
+    
