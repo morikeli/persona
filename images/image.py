57d94070c7c4704bc495d149b341592faa14eb96
magnus.coder <kelimbth@gmail.com>
2025-02-12 13:29:05 +0300

    deleted function update_avatar_image() & added whitespace in line 7

--- images/image.py
+++ images/image.py
@@ -1,17 +1,9 @@
 import streamlit as st
 
 
-def update_avatar_image(img):
-    """ This is a function that displays the generated avatar. """
-
-    img_col = st.columns([.4, .5, .4])
-    img_col[1].image(img, caption='Your avatar', use_column_width='auto')   # wrap the image in the center column
-    
-    return img
-
 def download_avatar(avatar_image):
     """ This function allows one to download their avatars."""
-
+    
     with open(avatar_image, 'rb') as image_file:
         st.download_button(
             label='Download avatar',
